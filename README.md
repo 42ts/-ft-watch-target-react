ac736cd5c2de46cd44984a5a233e57ba73566b72
Juyeong Maing <mjy9088@naver.com>
2023-09-26 23:52:50 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# `watch-target-react` - Util watchTarget wrapper for React
+
+React wrapper of util watchTarget
+
+## Usage
+
+```jsx
+function Portal({ children }) {
+  const { addComponent } = useContext(PortalContext);
+  const childrenWatchTarget = useWatchTarget(children);
+  useEffect(
+    () => addComponent(toComponent(childrenWatchTarget)),
+    [addComponent]
+  );
+  return null;
+}
+
+function toComponent(childrenWatchTarget) {
+  return function Component() {
+    return <>{useWatchValue(childrenWatchTarget)}</>;
+  };
+}
+```
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { usePersist } from '@-ft/use-persist';
+import { watchTarget, WatchTarget } from '@-ft/watch-target';
+import { useEffect, useState } from 'react';
+
+function getFromWatch<T>(watch: WatchTarget<T>['watch']): T {
+  let result: T;
+  watch((value) => {
+    result = value;
+  })();
+  return result!;
+}
+
+export function useWatchValue<T>(watch: WatchTarget<T>['watch']): T {
+  const [result, setResult] = useState(() => getFromWatch(watch));
+  useEffect(() => watch(setResult), [watch]);
+  return result;
+}
+
+export function useWatchTarget<T>(value: T): WatchTarget<T> {
+  const resultPersist = usePersist(() => watchTarget(value));
+  useEffect(() => resultPersist.current.set(value), [value]);
+  return resultPersist.current;
+}
